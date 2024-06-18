dd4203f217a581297329e28b52ac620058a46b45
MarkHunn <79857587+Manisean@users.noreply.github.com>
2024-06-18 10:33:33 -0400

    Add files via upload

---
+++ Flash2/flashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  flashViewController.swift
+//  Flash2
+//
+//  Created by Mark Hunnewell on 3/21/24.
+//
+
+import UIKit
+
+class FlashViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+}
